3d2ea59a1bd9776127f1be431a0be6238fad2a0e
Trent Henderson <trent.henderson1@outlook.com>
2023-02-09 21:15:20 +1100

    feature calcs

--- setup.R
+++ setup.R
@@ -23,6 +23,7 @@ library(correctR)
 # Create important folders if none exist
 
 if(!dir.exists('data')) dir.create('data')
+if(!dir.exists('data/feature-calcs')) dir.create('data/feature-calcs')
 if(!dir.exists('analysis')) dir.create('analysis')
 if(!dir.exists('output')) dir.create('output')
 if(!dir.exists('R')) dir.create('R')
