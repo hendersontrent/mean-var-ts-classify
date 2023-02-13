68a08e591dcb08eccd5e02f43d54bba0b8e22fc9
Trent Henderson <trent.henderson1@outlook.com>
2023-02-13 18:00:57 +1100

    NaN catch

--- analysis/compute-catch24.R
+++ analysis/compute-catch24.R
@@ -18,6 +18,8 @@ load("data/TimeSeriesData.Rda")
 #------------- Feature extraction --------------
 
 #' Function to map over datasets to avoid massive dataframe processing times / crashes
+#' 
+#' @importFrom purrr map_dfr
 #' @param data \code{data.frame} containing raw time series
 #' @param theproblem \code{string} specifying the problem to calculate features for
 #' @returns an object of class dataframe
@@ -33,11 +35,21 @@ extract_features_by_problem <- function(data, theproblem){
   tmp <- data %>%
     filter(problem == theproblem)
     
-    # Calculate features
+  # Calculate features
     
-    outs <- calculate_features(tmp, id_var = "id", time_var = "timepoint", 
-                               values_var = "values", group_var = "target", 
-                               feature_set = "catch22", catch24 = TRUE, seed = 123)[[1]]
+  outs <- calculate_features(tmp, id_var = "id", time_var = "timepoint", 
+                             values_var = "values", group_var = "target", 
+                             feature_set = "catch22", catch24 = TRUE, seed = 123)[[1]]
+    
+  # Catch cases where appended NAs cause errors (i.e., different time series have different lengths)
+  # We do this by mapping over IDs to a modified feature calculation function that drops NAs by ID
+  
+  if(length(unique(tmp$id)) != length(unique(outs$id))){
+    outs <- unique(tmp$id) %>%
+      purrr::map_dfr(~ calculate_features2(tmp, id_var = "id", time_var = "timepoint", 
+                                           values_var = "values", group_var = "target", 
+                                           catch24 = TRUE, seed = 123, the_id = .x))
+  }
 
   save(outs, file = paste0("data/feature-calcs/", theproblem, ".Rda"))
 }
